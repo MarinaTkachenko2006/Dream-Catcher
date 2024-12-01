8e280325be0fae6dce033470b4f37360a3295815
Ростислав Шведов <rustysquezzy@outlook.com>
2024-12-01 18:25:45 +0300

    Created a pause menu of the game.
    I created a menu that appears during the pause of the game, which works on all levels. I also changed the UI design (so far only for the pause menu, changing the design of the main menu in coordination with the team leader).

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using UnityEditor;
+// using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
