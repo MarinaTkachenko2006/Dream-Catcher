f38086a5186da5d2c943184cdd024802ede5a86f
Ростислав Шведов <rustysquezzy@outlook.com>
2025-05-04 23:56:58 +0300

    sorry for errors((.....

--- Assets/Scripts/ItemGiverController.cs
+++ Assets/Scripts/ItemGiverController.cs
@@ -2,50 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemGiverController : MonoBehaviour, Interactable
+public class ItemGiverController : MonoBehaviour
 {
-    [SerializeField] Dialog dialog1;
-    [SerializeField] Dialog dialog2;
-    [SerializeField] private Dialog dialogFirstItem;
-    [SerializeField] private Dialog dialogLastItem;
-
-    public string ItemToGive;
-    //public bool itemIsGiven = false;
-    AudioManager audioManager;
-    InventoryManager inventoryManager;
-
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
-        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
-        inventoryManager = InventoryManager.Instance;
-    }
-    public void Interact()
-    {
-        if (!inventoryManager.HasItem(ItemToGive))
-        {
-            StartCoroutine(InteractRoutine());
-        }
-        else
-        {
-            Debug.Log("Предмет уже получен");
-        }
+        
     }
 
-    IEnumerator InteractRoutine()
+    // Update is called once per frame
+    void Update()
     {
-        yield return StartCoroutine(DialogManager.Instance.ShowDialog(dialog1));
-        audioManager.PlaySFX(audioManager.item);
-        yield return StartCoroutine(DialogManager.Instance.ShowDialog(dialog2));
-        if (inventoryManager.ItemsCount() == 0)
-        {
-            yield return StartCoroutine(DialogManager.Instance.ShowDialog(dialogFirstItem));
-        }
-        else if (inventoryManager.ItemsCount() == 4)
-        {
-            dialogLastItem.Lines[0] = "*Вы почувствовали, что ранее запертая дверь в междусновье открылась.*";
-            yield return StartCoroutine(DialogManager.Instance.ShowDialog(dialogLastItem));
-        }
-        inventoryManager.AddItem(ItemToGive);
-        //itemIsGiven = true;
+        
     }
 }
